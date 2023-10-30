ec8a6306cdc73d0abbb4d657db556260f0f7009f
Your Name <you@example.com>
2023-10-30 12:20:53 -0300

    Version 3.0

--- W0490903_PROG1700_Fried_Chicken_Dinosaur.py
+++ W0490903_PROG1700_Fried_Chicken_Dinosaur.py
@@ -2,11 +2,13 @@
 Author: Zachary Mason
 Student ID: W0490903
 Course: PROG1700
-Date: 2023-10-23
+Date: 2023-10-30
 Project: Fried Chicken Dinosaur
 Repository: https://github.com/W0490903/PROG1700/
 Programming Language: Python 3
 License: Creative Commons
+Version: 3.0
+Lines of Code: 22
 """
 
 # Assign global variables.
@@ -39,30 +41,24 @@ print(""" _____                                                 _____
 while friedchicken_remaining > 0:
     # If it is the first day, the dinosaur only eats 1 pound.
     if days == 1:
-        print(f"It is day {days} and I'm Hungry. Let's Eat!")
         friedchicken_remaining = friedchicken_remaining - friedchicken_eaten
         days += 1
-        print(f"Chicken eaten today: {friedchicken_eaten:.2f}")
-        print(f"Chicken remaining: {friedchicken_remaining:.2f}")
+        print(f"It is day 1 and I'm Hungry. Let's Eat!\nChicken eaten today: {friedchicken_eaten:.2f}\nChicken remaining: {friedchicken_remaining:.2f}\n")
         # Increment the amount the dinosaur eats after each day, except the seventh.
         friedchicken_eaten += 0.05
     # On all other days except the seventh, the dinosaur eats 0.05 more than the previous day.
     elif days > 1 and days != 7 and days != 16:
-        print(f"It is day {days} and I'm Hungry. Let's Eat!")
-        print(f"Chicken eaten today: {friedchicken_eaten:.2f}")
         friedchicken_remaining = friedchicken_remaining - friedchicken_eaten
-        print(f"Chicken remaining: {friedchicken_remaining:.2f}")
+        print(f"It is day {days} and I'm Hungry. Let's Eat!\nChicken eaten today: {friedchicken_eaten:.2f}\nChicken remaining: {friedchicken_remaining:.2f}\n")
         friedchicken_eaten += 0.05
         days += 1
     # On the seventh day, the dinosaur does not eat any chicken.
     elif days == 7:
         days += 1
-        print(f"It is day 7 and I have a tummy ache. I can't eat anything today.")
+        print(f"It is day 7 and I have a tummy ache. I can't eat anything today.\n")
     # On day sixteen, he eats the last of his chicken, so this sets the amount remaining to zero.
     elif days == 16:
-        print(f"It is day {days} and I'm Hungry. Let's Eat!")
-        print(f"Chicken eaten today: 1.45")
-        print(f"Chicken remaining: 0")
+        print(f"It is day {days} and I'm Hungry. Let's Eat!\nChicken eaten today: 1.45\nChicken remaining: 0\n")
         friedchicken_remaining = 0
         
 # If the dinosaur runs out of chicken, prints the current day, and tells user that he has run out.
